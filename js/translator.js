9a39bb731fdb8a2a0c5480e206c3913830b493e0
Lito <xritoxrito@gmail.com>
2026-03-31 23:20:18 +0900

    Create translator.js

---
+++ js/translator.js
@@ -0,0 +1,97 @@
+translation = {
+  /* 敬称 */
+  "mr. ": "みすたー・", "ms. ": "みす・", "mrs. ": "みせす・", "mx. ": "みくす・",
+  "dr. ": "どくたー・", "prof. ": "ぷろふぇそる・",
+  /* アポストロフィたち */
+  "t's": "つ", "'s": "ず", 
+  /* 変なやつら */
+  "augh": "aう", "ough": "oー",
+  "ould": "uっど",
+  /* 母音 + w */
+  "aw": "oー", "ow": "oう",
+  /* 二重母音 */
+  "ai": "eい",
+  "ee": "iー", "ea": "iあ", "eu": "uー",
+  "oa": "oー", "oo": "uー",
+  /* 二重子音 */
+  "bb": "b", "dd": "d", "ff": "f", "gg": "g",
+  "kk": "k", "ll": "l", "mm": "m", "nn": "n", "pp": "p",
+  "ss": "s", "tt": "t", "vv": "v", "xx": "x", "zz": "z",
+  /* その他子音 + 母音 */
+  "tcha": "っちゃ", "tche": "っちぇ", "tchi":"っち", "tcho": "っちょ", "tchu": "っちゅ",
+  "cka": "っか", "cke": "っけ", "cki":"っき", "cko": "っこ", "cku": "っく",
+  "twa": "とぅわ", "twe": "とぅうぇ", "twi":"とぅうぃ", "two": "とぅー", "twu": "とぅう",
+  /* h子音 + 母音 */
+  "cha": "ちゃ", "che": "ちぇ", "chi":"ち", "cho": "ちょ", "chu": "ちゅ",
+  "gha": "が", "ghe": "げ", "ghi":"ぎ", "gho": "ご", "ghu": "ぐ",
+  "pha": "ふぁ", "phe": "ふぇ", "phi":"ふぃ", "pho": "ふぉ", "phu": "ふ",
+  "sha": "しゃ", "she": "しぇ", "shi":"し", "sho": "しょ", "shu": "しゅ",
+  "tha": "さ", "the": "ぜ", "thi": "すぃ", "tho": "ぞ", "thu": "す",
+  "wha": "わ", "whe": "うぇ", "whi":"うぃ", "who": "ほ", "whu": "う",
+  /* 単子音 + 母音 */
+  "ba": "ば", "be": "べ", "bi":"び", "bo": "ぼ", "bu": "ぶ",
+  "ca": "か", "ce": "せ", "ci":"し", "co": "こ", "cu": "く",
+  "da": "だ", "de": "で", "di":"でぃ", "do": "ど", "du": "どぅ",
+  "fa": "ふぁ", "fe": "ふぇ", "fi":"ふぃ", "fo": "ふぉ", "fu": "ふ",
+  "ga": "が", "ge": "じぇ", "gi":"ぎ", "go": "ご", "gu": "ぐ",
+  "ha": "は", "he": "へ", "hi":"ひ", "ho": "ほ", "hu": "ふ",
+  "ja": "じゃ", "je": "じぇ", "ji":"じ", "jo": "じょ", "ju": "じゅ",
+  "ka": "か", "ke": "け", "ki":"き", "ko": "こ", "ku": "く",
+  "la": "ら", "le": "れ", "li":"り", "lo": "ろ", "lu": "る",
+  "ma": "ま", "me": "め", "mi":"み", "mo": "も", "mu": "む",
+  "na": "な", "ne": "ね", "ni":"に", "no": "の", "nu": "ぬ",
+  "pa": "ぱ", "pe": "ぺ", "pi":"ぴ", "po": "ぽ", "pu": "ぷ",
+  "qa": "か", "qe": "け", "qi":"き", "qo": "こ", "qu": "く",
+  "ra": "ら", "re": "れ", "ri":"り", "ro": "ろ", "ru": "る",
+  "sa": "さ", "se": "せ", "si":"すぃ", "so": "そ", "su": "す",
+  "ta": "た", "te": "て", "ti":"てぃ", "to": "と", "tu": "とぅ",
+  "va": "ゔぁ", "ve": "ゔぇ", "vi":"ゔぃ", "vo": "ゔぉ", "vu": "ゔ",
+  "wa": "わ", "we": "うぇ", "wi":"うぃ", "wo": "うぉ", "wu": "う",
+  "xa": "ざ", "xe": "ぜ", "xi":"くし", "xo": "くそ", "xu": "くす",
+  "ya": "や", "ye": "いぇ", "yi":"い", "yo": "よ", "yu": "ゆ",
+  "za": "ざ", "ze": "ぜ", "zi":"ずぃ", "zo": "ぞ", "zu": "ず",
+  /* 子音 + y */
+  "tchy": "っちー", "cky": "っきー", "twy": "とぅいー",
+  "chy": "ちー", "phy": "ふぃー", "shy": "しゃい", "thy": "すぃー", "why": "わい",
+  "by": "ばい", "cy": "さい", "dy": "だい", "fy": "ふぁい", "gy": "じー", "hy": "はい", "jy": "じゃい",
+  "ky": "きー", "ly": "りー", "my": "まい", "ny": "にー", "py": "ぱい", "ry": "りー", "sy": "すぃー",
+  "ty": "てぃー", "vy": "ゔぃー", "wy": "うぃー", "xy": "ざい","zy": "ずぃー",
+  /* その他子音単品 */
+  "tch": "っち",
+  "ck":"っく", "ts":"つ",
+  "ch": "ち", "gh": "ふ", "ph": "ふ", "sh": "しゅ", "th": "す", "wh": "う",
+  /* 母音単品 */
+  "a": "あ", "e": "え", "i": "い", "o": "お", "u": "う",
+  /* 子音単品 */
+  "b": "ぶ", "c": "く", "d": "ど", "f": "ふ", "g": "ぐ", "h": "は", "j": "じゅ",
+  "k": "く", "l": "る", "m": "む", "n": "ん", "p": "ぷ", "q": "く", "r": "る",
+  "s": "す", "t": "と", "v": "ゔ", "w": "う", "x": "っくす", "y": "い", "z": "ず",
+  /* 記号 */
+  "...": "…", "..": "‥",
+  "'": "", ", ": "、", ". ": "。",
+  ",": "、", ".": "。",
+}
+const pre = get("#pre")
+const pos = get("#pos")
+pre.addEventListener("keyup", translate)
+function translate() {
+  let result = pre.value.toLowerCase()
+  for (let i = 0; i != Object.keys(translation).length; i++) {
+    let tr = (typeof Object.entries(translation)[i][1] == "object") ? Object.entries(translation)[i][1][Math.floor(Math.random() * Object.entries(translation)[i][1].length)] : Object.entries(translation)[i][1]
+    result = result.replaceAll(Object.entries(translation)[i][0], tr)
+  }
+  pos.value = result
+}
+window.addEventListener("DOMContentLoaded", () => {
+  const s = location.search
+  const q = new URLSearchParams(s)
+  if (s.includes("text=") == true) {
+    try {
+      pre.value = atob(q.get("text"))
+      translate()
+      console.log("読み込み成功")
+    } catch (err) {
+      console.error("読み込み失敗、" + err)
+    }
+  }
+})
