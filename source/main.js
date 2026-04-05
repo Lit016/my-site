e9ce10f83b75788451fc7b3bbd2cc464ac19424b
lit016 <xritoxrito@gmail.com>
2026-04-05 04:41:45 +0000

    New garbage

--- source/main.js
+++ source/main.js
@@ -5,20 +5,27 @@ function getAll(thing) {
   return document.querySelectorAll(thing);
 }
 
-function copy(id, text) {
-  let old = get(id).innerHTML
+function copy(text, id) {
+  let old = "undefined"
+  if ((typeof id) != "undefined") {
+    old = get(id).innerHTML
+  }
   try {
     navigator.clipboard.writeText(text);
-    get(id).innerHTML = `<i class="fa-solid fa-check"></i>`
-    setTimeout(() => {
-      get(id).innerHTML = old
-    }, 500)
+    if (old != "undefined") {
+      get(id).innerHTML = `<i class="fa-solid fa-check"></i>`
+      setTimeout(() => {
+        get(id).innerHTML = old
+      }, 500)
+    }
     console.log("コピー成功")
   } catch (err) {
-    get(id).innerHTML = `<i class="fa-solid fa-ban"></i>`
-    setTimeout(() => {
-      get(id).innerHTML = old
-    }, 500)
-    console.error("コピー失敗、", err);
+    if (old != "undefined") {
+      get(id).innerHTML = `<i class="fa-solid fa-ban"></i>`
+      setTimeout(() => {
+        get(id).innerHTML = old
+      }, 500)
+      console.error("コピー失敗、", err);
+    }
   }
 }
